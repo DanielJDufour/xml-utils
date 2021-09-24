1449f52190618e48bb2829dc1a0b2736bef14550
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-24 14:55:49 -0400

    added getAttribute to index.js output

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const findTagByPath = require("./find-tag-by-path");
 const findTagsByPath = require("./find-tags-by-path");
 
 module.exports = {
+  getAttribute,
   findTagByName,
   findTagsByName,
   findTagByPath,
