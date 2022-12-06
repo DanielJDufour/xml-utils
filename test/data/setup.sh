447ba7ab2c25efd5c93193262439802f4db23a2d
DanielJDufour <daniel.j.dufour@gmail.com>
2022-12-06 08:46:43 -0500

    added example.svg and tmx.xml to test data download script

--- test/data/setup.sh
+++ test/data/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 
+wget https://xml-utils.s3.amazonaws.com/example.svg
 wget https://xml-utils.s3.amazonaws.com/gadas-export.png.aux.xml
 wget https://xml-utils.s3.amazonaws.com/iso.xml
 wget https://xml-utils.s3.amazonaws.com/rgb_raster.tif.aux.xml
 wget https://xml-utils.s3.amazonaws.com/m_3008501_ne_16_1_20171018.mrf
+wget https://xml-utils.s3.amazonaws.com/tmx.xml
